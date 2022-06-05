e72e2b568e96203e7c2f32e6f4a10817848fa7b0
Harisankar P S <mailme@hsps.in>
2022-06-05 16:34:28 -0700

    Link to github.com sourcecode

--- src/components/TodosFooter.vue
+++ src/components/TodosFooter.vue
@@ -1,8 +1,7 @@
 <template>
  <footer class="info">
       <p>
-        Created by
-        <a href="https://hsps.in">Harisankar P S</a>
+        Author <a href="https://hsps.in">Harisankar P S</a> | <a href="https://github.com/coderhs/new-tab-reminders">Source Code</a>
       </p>
     </footer>
 </template>
